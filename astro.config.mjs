23e2e7834e5c8020786145caa7f5f83d5acf05e0
Lautaro <lautaroceballos2013@gmail.com>
2026-05-04 13:16:49 -0300

    chore: prepare for GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://argentinadelsur.com.ar',
+  site: 'https://LautaroCeballos.github.io',
+  base: '/argentino-del-sur',
   i18n: {
     defaultLocale: 'es',
     locales: ['es', 'pt'],
